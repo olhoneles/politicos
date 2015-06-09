e4887e312b5fd8b7e01cbf847460368d3ab044de
Marcelo Jorge Vieira <metal@alucinados.com>
2015-06-08 22:21:07 -0300

    PoliticalParty: Fixed siglum size

---
+++ politicos/migrations/versions/1f943fff87a9_politicalparty_fixed_siglum_size.py
@@ -0,0 +1,26 @@
+"""PoliticalParty: Fixed siglum size
+
+Revision ID: 1f943fff87a9
+Revises: 3c20fed5a17f
+Create Date: 2015-06-08 22:05:48.444754
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1f943fff87a9'
+down_revision = '3c20fed5a17f'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.alter_column(
+        'political_party', 'siglum', type_=sa.String(15), nullable=False
+    )
+
+
+def downgrade():
+    op.alter_column(
+        'political_party', 'siglum', type_=sa.String(2048), nullable=False
+    )
